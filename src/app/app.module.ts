b73295104996eb1fd045eb365686240bbcbbb977
teacher 01 <teacher.bt@betimes.biz>
2023-01-24 09:13:20 +0700

    new24/1/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,7 @@ import { AppComponent } from './app.component';
 import { FeatherModule } from 'angular-feather';
 import { LogOut } from 'angular-feather/icons';
 
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 
 import { LoginComponent } from './component/main/login/login.component';
 import { NavbarComponent } from './component/main/navbar/navbar.component';
@@ -31,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListDataComponent } from './component/user-list-data/user-list-data.component';
 import { FooterComponent } from './component/main/footer/footer.component';
 import { FoodListMenuComponent } from './component/food-list-menu/food-list-menu.component';
+import { FoodListPopupComponent } from './component/food-list-popup/food-list-popup.component';
 const icons = {
 
   LogOut
@@ -46,6 +33,7 @@ const icons = {
     UserListDataComponent,
     FooterComponent,
     FoodListMenuComponent,
+    FoodListPopupComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,21 +41,6 @@ const icons = {
     FormsModule,
     HttpClientModule,
     FeatherModule.pick(icons),
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
     BrowserAnimationsModule,
   ],
   providers: [],
