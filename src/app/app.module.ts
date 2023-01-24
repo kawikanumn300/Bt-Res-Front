6654c56d4a9f4017de31e9265eaac91bc965c454
teacher 01 <teacher.bt@betimes.biz>
2023-01-24 16:21:51 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,7 @@ import { AppComponent } from './app.component';
 import { FeatherModule } from 'angular-feather';
 import { LogOut } from 'angular-feather/icons';
 
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 
 import { LoginComponent } from './component/main/login/login.component';
 import { NavbarComponent } from './component/main/navbar/navbar.component';
@@ -53,21 +39,7 @@ const icons = {
     FormsModule,
     HttpClientModule,
     FeatherModule.pick(icons),
-    MdbAccordionModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
-    MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
-    MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
+    
     BrowserAnimationsModule,
   ],
   providers: [],
