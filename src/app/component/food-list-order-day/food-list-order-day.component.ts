d8c552b2e1cf455b8fc6799e15d55a4d3e9665eb
teacher 01 <teacher.bt@betimes.biz>
2023-02-02 09:14:08 +0700

    add display data

--- src/app/component/food-list-order-day/food-list-order-day.component.ts
+++ src/app/component/food-list-order-day/food-list-order-day.component.ts
@@ -1,10 +1,10 @@
+import { BtResFoodList, foodlisturl } from 'src/app/service/BtResFoodListService';
 import { HttpClient } from '@angular/common/http';
 import { Component, PipeTransform, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BtResUserBill, userbill } from 'src/app/service/BtResUserBillService';
 import { BtResUser, baseUrl } from 'src/app/service/BtResUserService';
 
-import { combineLatest } from 'rxjs';
 
 
 @Component({
@@ -12,38 +12,37 @@ import { combineLatest } from 'rxjs';
   templateUrl: './food-list-order-day.component.html',
   styleUrls: ['./food-list-order-day.component.scss']
 })
-export class FoodListOrderDayComponent implements OnInit{
-constructor(private http:HttpClient){
-}
-	filter = new FormControl('', { nonNullable: true });
-  billdata:any;
+export class FoodListOrderDayComponent implements OnInit {
+  constructor(private http: HttpClient) {
+  }
+  filter = new FormControl('', { nonNullable: true });
+  billdata: any;
   datenow = Date();
-  userdata:any;
-  userid:any
-  usershow:any
-  joinedData:any
+  userdata: any;
+  userid: any
+  usershow: any
+  fooddata: any
   ngOnInit(): void {
-    this.http.get<BtResUserBill>(userbill).subscribe(response=>{
-      this.billdata= response.Value
+    this.http.get<BtResUserBill>(userbill).subscribe(response => {
+      this.billdata = response.Value
       console.log(this.billdata)
     });
 
-    this.http.get<BtResUser>(baseUrl).subscribe(response=>{
-      this.userdata= response.Value
+    this.http.get<BtResUser>(baseUrl).subscribe(response => {
+      this.userdata = response.Value
       console.log(this.userdata)
     });
-
-  
-  }
-
-    // await this.http.get<BtResUser>(baseUrl+'/').subscribe(response=>{
-    //   this.userdata = response.Value
-    //   console.log(this.userdata.USER_USERNAME)
-
-    // })
-
+    this.http.get<BtResFoodList>(foodlisturl).subscribe(response => {
+      this.fooddata = response.Value
+      console.log(this.fooddata)
+    });
 
   }
+
+
+
+
+}
 
 
 
